383086bd8dffb10d5a5a0a8a332f44945ae81009
AjayFrancisTechversant <ajay.francis@techversantinfotech.com>
2024-08-02 10:17:59 +0530

    finalise

--- src/Modules/HomeScreen/index.tsx
+++ src/Modules/HomeScreen/index.tsx
@@ -1,11 +1,11 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 import {useScreenContext} from '../../Contexts/ScreenContext';
-import styles1 from './style';
+import styles from './style';
 
 const HomeScreen = () => {
   const screenContext = useScreenContext();
-  const screenStyles = styles1(
+  const screenStyles = styles(
     screenContext.isPortrait ? screenContext.height : screenContext.width,
     screenContext.isPortrait ? screenContext.width : screenContext.height,
     screenContext.isPortrait,
